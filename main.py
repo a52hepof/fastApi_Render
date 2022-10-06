8447c80318ec84501d819fd23db81daa5ce05b60
Fernando Herrera Poch <37152969+a52hepof@users.noreply.github.com>
2022-10-06 15:20:04 +0200

    Update main.py
    testing ci/cd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+lfrom typing import Optional
 
 from fastapi import FastAPI
 
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello people"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
